522509cddd2ddbdfa266effcef8931be01bbd36d
Domingos Santos <domsantos@gmail.com>
2014-08-07 23:35:03 -0300

    JAnelas

---
+++ app/controller/HomeController.js
@@ -0,0 +1,13 @@
+/**
+ * Created by domingos on 8/7/14.
+ */
+Ext.define('CandidatoOnline.controller.HomeController', {
+    extend : 'Ext.app.Controller',
+
+    config:{
+        refs:{
+            mainView: "#homeView"
+
+        }
+    }
+});
---
+++ app/view/MenuView.js
@@ -0,0 +1,47 @@
+/**
+ * Created by domingos on 8/7/14.
+ */
+/**
+ * Created by domingos on 8/7/14.
+ */
+Ext.define('CandidatoOnline.view.MenuView',{
+    extend: 'Ext.Container',
+    xtype: 'menuView',
+    id: 'menuView',
+
+    requires: [
+        'Ext.XTemplate'
+    ],
+
+    config:{
+        layout: {
+            type: 'vbox',
+            align: 'center'
+        },
+        scrollable: 'vertical',
+        items:[
+            {
+                xtype: 'titlebar',
+                docked: 'top',
+                title: 'Candidato Online'
+            },
+            {
+                id: 'btPesquisa',
+                xtype: 'button',
+                ui: 'confirm',
+                width: '70%',
+                text: 'Pesquisa'
+            },
+            {
+                id: 'btContas',
+                xtype: 'button',
+                ui: 'confirm',
+                width: '70%',
+                text: 'Contas'
+            }
+
+        ]
+    }
+
+
+});
